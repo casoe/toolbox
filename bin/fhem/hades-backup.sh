9823beb8aaf647d9eb63d19ee9187a933f4baeaf
Carsten Söhrens <soehrens@gmail.com>
2020-02-19 19:55:35 +0100

    Anpassung hades-backup auf das neue NAS

--- bin/fhem/hades-backup.sh
+++ bin/fhem/hades-backup.sh
@@ -1,3 +1,3 @@
-sudo mount -t cifs -o credentials=~/.cifs //morpheus/shares/other/backup /mnt/backup
+mount /mnt/backup
 sudo raspiBackup.sh -a : -o : -m detailed
-sudo umount /mnt/backup
+umount /mnt/backup
