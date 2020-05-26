66cf07be58ddc24e1d30000254ebae70fc4e9c9a
Carsten Söhrens <soehrens@gmail.com>
2020-05-26 10:23:50 +0200

    Optimierungen am Logging für Tages-Backup

--- bin/fhem/cron_daily-backup.sh
+++ bin/fhem/cron_daily-backup.sh
@@ -9,6 +9,7 @@
 # 07.01.2020 Stoppen der Zeit ergänzt
 # 23.05.2020 Fix in Bezug auf mount mit sudo
 # 25.05.2020 Fix an Permissions-Error für rsync; delete backups older than 14 days locally
+# 26.05.2020 Logging angepasst
 
 PGDUMP="/usr/bin/pg_dump"
 PSQL="/usr/bin/psql"
@@ -20,10 +21,21 @@ DBTARGET="/home/pi/backup/db/db_backup_$TODAY.sqlc"
 ZIPTARGET="/home/pi/backup/conf/fhem_backup_$TODAY.zip"
 RSYNC="/usr/bin/rsync"
 
-# Startzeit speichern
+### Funktion zum effizienteren Logging
+log() {
+	if [[ -z "$LOG" ]]; then
+			echo "[$(date +%Y-%m-%d\ %H:%M:%S)]: ERROR log variable in $0 not defined" 1>&2
+			exit 1
+	fi
+
+	echo "[$(date +%Y-%m-%d\ %H:%M:%S)]: $*" >> $LOG 2>&1
+}
+
+### Startzeit speichern
 START=$(date +%s)
 
 ### Löschen von Einträgen für Power, die älter als 7 Tage sind
+log "INFO" "Löschen von Einträgen für Power, die älter als 7 Tage sind"
 $PSQL $DBNAME << EOF > $LOG 2>&1
 delete from history where reading='ENERGY_Power' and (timestamp < now() - interval '7 days');
 delete from history where reading='power' and (timestamp < now() - interval '7 days');
@@ -33,10 +45,11 @@ select count(*) from history;
 EOF
 
 ### Dump der Postgres-Datenbank
+log "INFO" "Dump der Postgres-Datenbank"
 $PGDUMP -v -Fc --file=$DBTARGET $DBNAME >> $LOG 2>&1
 
 ### Wiederherstellen-Hinweis im Logfile
-echo "Wiederherstellen: pg_restore -Fc -v --clean -h localhost -U fhem -d fhem $DBTARGET" >> $LOG 2>&1
+log "INFO" "Wiederherstellen: pg_restore -Fc -v --clean -h localhost -U fhem -d fhem $DBTARGET"
 
 ### Archivieren des Skripts und der Config-Dateien
 $ZIP -rv $ZIPTARGET /opt/fhem/fhem.cfg >> $LOG 2>&1
@@ -45,10 +58,8 @@ $ZIP -rv $ZIPTARGET /home/pi/.bash_history >> $LOG 2>&1
 $ZIP -rv $ZIPTARGET /opt/fhem/www/gplot/myPlot*.gplot >> $LOG 2>&1
 $ZIP -rv $ZIPTARGET /opt/fhem/www/tablet/index.html >> $LOG 2>&1
 
-### Rotation: Delete backups older than 14 days locally
-echo "### Rotation: Delete backups older than 5 days"
-### For the sake of comprehensiveness try to delete everything from today -15 to today -30
-### days
+### Delete everything locally from today -15 to today -30 days
+log "INFO" "Delete everything locally from today -15 to today -30 days"
 for DAYBACK in {15..30}; do
 	DATEBACK=$(date --date "- $DAYBACK day" +%F)
 	rm -rf /home/pi/backup/db/db_backup_$DATEBACK*
@@ -57,6 +68,7 @@ for DAYBACK in {15..30}; do
 done
 
 ### rsync zum NAS
+log "INFO" "rsync zum NAS"
 if [ ! $(mount | grep -o /mnt/backup ) ]; then
   sudo mount /mnt/backup
 fi
@@ -64,4 +76,4 @@ sudo $RSYNC -avht --update --no-o --no-g --no-perms /home/pi/backup/ /mnt/backup
 sudo umount /mnt/backup
 
 ### Backup-Zeit ausgeben
-echo Backup time: `date -u -d "0 $(date +%s) seconds - $START seconds" +"%H:%M:%S"` >> $LOG 2>&1
+log "INFO" "Backup time $(date -u -d "0 $(date +%s) seconds - $START seconds" +"%H:%M:%S")"
