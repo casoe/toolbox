641f7b3d8391318e1e9f555b0087dc11213075d1
Carsten Söhrens <soehrens@gmail.com>
2022-02-19 12:16:22 +0100

    Noch die Variablen-Namen verbessert

--- bin/fhem/cron_calc-and-message-daily.sh
+++ bin/fhem/cron_calc-and-message-daily.sh
@@ -8,7 +8,7 @@
 # 25.02.2020 delete-Statement für die Tabelle current ergänzt, Debug echo-Statements entfernt
 # 10.01.2022 Format für Angabe kW/h auf drei Nachkommastellen begrenzt
 # 31.01.2022 delete-Statement für fehlerhafte Daten von ENERGY_Yesterday ergänzt
-# 16.02.2022 Variablen für YES und DBY umbenannt
+# 16.02.2022 Variablen für YDA und DBY umbenannt
 
 TOKEN=312020795:AAHF3Uc_5L6mcn9hlo7oEhaDfZ5ypWCc3Mk
 CHAT_ID=9437849
@@ -16,18 +16,18 @@ URL="https://api.telegram.org/bot$TOKEN/sendMessage"
 PSQL="/usr/bin/psql"
 DBNAME="postgresql://fhem:fhem@localhost:5432/fhem"
 
-# Das Datum von gestern (YES) und vorgestern (DBY) berechnen
+# Das Datum von gestern (YDA) und vorgestern (DBY) berechnen
 YESTERDAY=`date -d "1 day ago" '+%d.%m.%Y'`
-YES_ISO=`date -d "1 day ago" '+%Y-%m-%d'`
+YDA_ISO=`date -d "1 day ago" '+%Y-%m-%d'`
 DBY_ISO=`date -d "2 days ago" '+%Y-%m-%d'`
 
 # Fehlerhaft Daten für ENERGY_Yesterday am gestrigen und vorgestrigen Tag entfernen
 DELETE=$($PSQL -X $DBNAME -t -c "delete from history where date_trunc('day', timestamp)= '$DBY_ISO' and device = 'MQTT2_DVES_0371A9' and reading = 'ENERGY_Yesterday' and timestamp !=(select min(timestamp) from history where date_trunc('day', timestamp)= '$DBY_ISO' and device = 'MQTT2_DVES_0371A9' and reading = 'ENERGY_Yesterday');")
-DELETE=$($PSQL -X $DBNAME -t -c "delete from history where date_trunc('day', timestamp)= '$YES_ISO' and device = 'MQTT2_DVES_0371A9' and reading = 'ENERGY_Yesterday' and timestamp !=(select min(timestamp) from history where date_trunc('day', timestamp)= '$YES_ISO' and device = 'MQTT2_DVES_0371A9' and reading = 'ENERGY_Yesterday');")
+DELETE=$($PSQL -X $DBNAME -t -c "delete from history where date_trunc('day', timestamp)= '$YDA_ISO' and device = 'MQTT2_DVES_0371A9' and reading = 'ENERGY_Yesterday' and timestamp !=(select min(timestamp) from history where date_trunc('day', timestamp)= '$YDA_ISO' and device = 'MQTT2_DVES_0371A9' and reading = 'ENERGY_Yesterday');")
 
 # Abfrage der total_consumption für gestern und vorgestern aus der Datenbank
 # "select distinct on" bestimmt den ersten Tupel einer Abfrage
-TOTAL1=`$PSQL -X $DBNAME -t -c "SELECT DISTINCT ON (timestamp::date) value FROM history where reading='total_consumption' AND timestamp::date='$YES_ISO' ORDER BY timestamp::date DESC, timestamp DESC;"`
+TOTAL1=`$PSQL -X $DBNAME -t -c "SELECT DISTINCT ON (timestamp::date) value FROM history where reading='total_consumption' AND timestamp::date='$YDA_ISO' ORDER BY timestamp::date DESC, timestamp DESC;"`
 TOTAL2=`$PSQL -X $DBNAME -t -c "SELECT DISTINCT ON (timestamp::date) value FROM history where reading='total_consumption' AND timestamp::date='$DBY_ISO' ORDER BY timestamp::date DESC, timestamp DESC;"`
 
 # Verbrauch berechnen (dafür muss bc installiert sein -> apt install bc)
@@ -37,9 +37,9 @@ COSTS=`bc <<< "$CONSUMPTION*0.25"`
 # Alte Datensätze mit reading 'daily_consumption' als current entfernen
 # Verbrauch von gestern als zusätzliche Zeile in die Datenbank einfügen (current und history)
 $PSQL -X $DBNAME -t -c "delete from current where reading='daily_consumption';"
-$PSQL -X $DBNAME -t -c "insert into current (timestamp, device, type, event, reading, value, unit) values ('$(date -d "$YES_ISO" "+%Y-%m-%d 23:59:59")', 'Stromzaehler', 'OBIS', 'daily_consumption: $CONSUMPTION', 'daily_consumption', $CONSUMPTION, '');"
-$PSQL -X $DBNAME -t -c "delete from history where reading='daily_consumption' and timestamp >= '$YES_ISO';"
-$PSQL -X $DBNAME -t -c "insert into history (timestamp, device, type, event, reading, value, unit) values ('$(date -d "$YES_ISO" "+%Y-%m-%d 23:59:59")', 'Stromzaehler', 'OBIS', 'daily_consumption: $CONSUMPTION', 'daily_consumption', $CONSUMPTION, '');"
+$PSQL -X $DBNAME -t -c "insert into current (timestamp, device, type, event, reading, value, unit) values ('$(date -d "$YDA_ISO" "+%Y-%m-%d 23:59:59")', 'Stromzaehler', 'OBIS', 'daily_consumption: $CONSUMPTION', 'daily_consumption', $CONSUMPTION, '');"
+$PSQL -X $DBNAME -t -c "delete from history where reading='daily_consumption' and timestamp >= '$YDA_ISO';"
+$PSQL -X $DBNAME -t -c "insert into history (timestamp, device, type, event, reading, value, unit) values ('$(date -d "$YDA_ISO" "+%Y-%m-%d 23:59:59")', 'Stromzaehler', 'OBIS', 'daily_consumption: $CONSUMPTION', 'daily_consumption', $CONSUMPTION, '');"
 
 # Bei Kosten und Verbrauch den Punkt (Decimal) durch Komma ersetzen
 # Sieht dann besser in der Telegram-Message aus
