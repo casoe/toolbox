3a5ce20c9fcddde2509caa137a889fbd8daf6930
Carsten Söhrens <casoe@gmx.de>
2022-04-14 13:04:42 +0200

    Zeitfenster auf 3 Tage reduziert

--- bin/synology/motioneye-sync.sh
+++ bin/synology/motioneye-sync.sh
@@ -6,10 +6,11 @@
 
 # Historie
 # 2022-03-17 Initiale Version
+# 2022-04-14 Zeitfenster auf 3 Tage reduziert
 
 ### Synce das Output-Verzeichnis auf MotionEye ohne Thumbnails
 rsync -av -e ssh --exclude '.keep'  --exclude '*.thumb' root@192.168.2.79:/data/output/Camera1/ /volume1/backup/motionseye/video/
 
-## Entferne alle Video kleiner als 5MB und älter als 7 Tage
-rm -rvf $(find /volume1/backup/motionseye/video/ -type f -size -5M -mtime +7)
+## Entferne alle Video kleiner als 5MB UND älter als 3 Tage
+rm -rvf $(find /volume1/backup/motionseye/video/ -type f -size -5M -mtime +3)
 
